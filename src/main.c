70cf9d1078e05b75e7670e9ef422c360177f904c
Matheus <matheus.guedes.mg.m@gmail.com>
2025-10-03 20:27:40 -0300

    fix:oops, I was using the wrong type of pointer

--- src/main.c
+++ src/main.c
@@ -112,9 +112,9 @@ void addr_to_str(struct sockaddr_storage *addr, socklen_t addr_len, char *addr_s
 
 	if (addr->ss_family == AF_INET) {
 		/* This is my greatest act of pointer magic to this date. */
-		raw_addr = &((struct sockaddr_in *)&addr)->sin_addr;
+		raw_addr = &((struct sockaddr_in *)addr)->sin_addr;
 	} else {
-		raw_addr = &((struct sockaddr_in6 *)&addr)->sin6_addr;
+		raw_addr = &((struct sockaddr_in6 *)addr)->sin6_addr;
 	}
 
 	inet_ntop(addr->ss_family, raw_addr, addr_str, addr_len);
